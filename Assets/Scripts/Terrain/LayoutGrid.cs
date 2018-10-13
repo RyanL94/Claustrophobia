4ac0ead5d22a2b9c9615bc07ae2132e6fe1c5579
Samuel Proulx <proulxsamuel@gmail.com>
2018-10-12 23:32:38 -0400

    Add floor tiles to layout

--- Assets/Scripts/Terrain/LayoutGrid.cs
+++ Assets/Scripts/Terrain/LayoutGrid.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 // Layout grid that the terrain system is based on.
 public class LayoutGrid : MonoBehaviour {
+    public GameObject groundTile; // tile that constitutes the ground
     public GameObject boundaryBlock; // block that delimits the playable zone
     public GameObject standardWallBlock; // standard wall block that the grid places
     protected Vector2Int gridSize; // size of the grid in cells
-    private GameObject[,] grid; // grid which contains the instances of blocks
+    protected GameObject[,] grid; // grid which contains the instances of blocks
     private HashSet<Vector2Int> unvisited; // list of unvisited cells (used for maze generation)
     private Transform ground; // parent object for ground tiles
     private Transform walls; // parent object for wall blocks
@@ -31,6 +32,7 @@ public class LayoutGrid : MonoBehaviour {
                 } else if (x % 2 == 0 || y % 2 == 0) {
                     Place(standardWallBlock, position);
                 } else {
+                    Place(groundTile, position, true);
                     unvisited.Add(position);
                 }
             }
@@ -42,22 +44,27 @@ public class LayoutGrid : MonoBehaviour {
         return new Vector3(position.x, 0.0f, position.y);
     }
 
-    // Place (an instance of) a block at the given position.
-    public void Place(GameObject block, Vector2Int position) {
-        Remove(position);
+    // Place (an instance of) a block or tile at the given position.
+    public void Place(GameObject block, Vector2Int position, bool onGround=false) {
+        Empty(position);
         var instance = Instantiate(block, ToWorldPosition(position), Quaternion.identity);
-        instance.transform.parent = walls;
+        instance.transform.parent = onGround? ground : walls;
         grid[position.x, position.y] = instance;
     }
 
     // Remove the block at the given position, if any.
     public void Remove(Vector2Int position) {
+        Place(groundTile, position, true);
+        unvisited.Remove(position);
+    }
+
+    // Empty the cell at the given position, destroying any block or tile.
+    public void Empty(Vector2Int position) {
         var instance = grid[position.x, position.y];
         if (instance != null) {
+            instance.transform.parent = null;
             Destroy(instance);
         }
-        grid[position.x, position.y] = null;
-        unvisited.Remove(position);
     }
 
     // Destroy all of the grid's block game objects.
@@ -66,7 +73,7 @@ public class LayoutGrid : MonoBehaviour {
             for (int x = 0; x < grid.GetLength(0); ++x) {
                 for (int y = 0; y < grid.GetLength(1); ++y) {
                     var position = new Vector2Int(x, y);
-                    Remove(position);
+                    Empty(position);
                 }
             }
         }
