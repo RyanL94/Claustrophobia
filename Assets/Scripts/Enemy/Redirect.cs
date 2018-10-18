726bf389a682efa0a1e25ffaa5575e6c435d5aa4
Ryan <rswllee@hotmail.com>
2018-10-18 19:24:25 -0400

    Organized file structure

---
+++ Assets/Scripts/Enemy/Redirect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Redirect : MonoBehaviour {
+
+    public Vector3 redirector;
+	void Start ()
+    {
+        if (transform.name == "WestRedirectors") redirector = new Vector3(1.0f, 0.0f, 0.0f);
+        else if (transform.name == "SouthRedirectors") redirector = new Vector3(0.0f, 0.0f, 1.0f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Baddie")
+        {
+            EnemyMovement toChange = other.GetComponent<EnemyMovement>();
+            toChange.direction = redirector;
+        }
+        
+    }
+}
