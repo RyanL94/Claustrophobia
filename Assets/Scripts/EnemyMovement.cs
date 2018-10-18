8a721d8a0319e416f1b592ecb21a551b98c4170b
steak-house <valp.perrot@gmail.com>
2018-10-18 18:40:05 -0400

    #1 : Added enemy movement in maze and redirection block behavior

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -16,6 +16,17 @@ public class EnemyMovement : MonoBehaviour
     float rayDistance;
     [SerializeField]
     float rotate;
+
+    //val stuff
+    public Vector3 direction;
+    public GameObject toChase;
+    public float speed;
+
+    static Vector3 destination;
+    private float distanceToTarget;
+
+    //together stuff
+    private bool inRoom = false;
 	
 	void Update ()
     {
@@ -23,6 +34,7 @@ public class EnemyMovement : MonoBehaviour
         {
             Pathfinding();
             Move();
+            
             //Turn();
         }
     }
@@ -51,46 +63,70 @@ public class EnemyMovement : MonoBehaviour
 
     void Pathfinding()
     {
-        float leftRay = 0.99f * rayDistance;
-        RaycastHit hit;
-        Vector3 raycastOffset = Vector3.zero;
-
-        Vector3 left = transform.position - transform.right * rayCastOffset;
-        Vector3 right = transform.position + transform.right * rayCastOffset;
-        Vector3 forward = transform.position + transform.forward * rayCastOffset;
-        Vector3 backward = transform.position - transform.forward * rayCastOffset;
-
-        Debug.DrawRay(left, transform.forward * rayDistance, Color.green);
-        Debug.DrawRay(right, transform.forward * rayDistance, Color.green);
-        Debug.DrawRay(forward, transform.forward * rayDistance, Color.green);
-        Debug.DrawRay(backward, -transform.forward * rayDistance, Color.green);
-
-        if(Physics.Raycast(left, transform.forward, out hit, leftRay))
+        if (inRoom == true)
         {
-            raycastOffset += Vector3.up;
-        }
-        else if(Physics.Raycast(right, transform.forward, out hit, rayDistance))
-        {
-            Debug.Log("Ray collision2");
-            raycastOffset -= Vector3.up;
-        }
+            float leftRay = 0.99f * rayDistance;
+            RaycastHit hit;
+            Vector3 raycastOffset = Vector3.zero;
 
-        if (Physics.Raycast(forward, transform.forward, out hit, rayDistance))
-        {
-            
-        }
-        else if (Physics.Raycast(backward, -transform.forward, out hit, rayDistance))
-        {
-            
-        }
+            Vector3 left = transform.position - transform.right * rayCastOffset;
+            Vector3 right = transform.position + transform.right * rayCastOffset;
+            Vector3 forward = transform.position + transform.forward * rayCastOffset;
+            Vector3 backward = transform.position - transform.forward * rayCastOffset;
 
-        if(raycastOffset != Vector3.zero)
-        {
-            transform.Rotate(raycastOffset * rotate * Time.deltaTime);
+            Debug.DrawRay(left, transform.forward * rayDistance, Color.green);
+            Debug.DrawRay(right, transform.forward * rayDistance, Color.green);
+            Debug.DrawRay(forward, transform.forward * rayDistance, Color.green);
+            Debug.DrawRay(backward, -transform.forward * rayDistance, Color.green);
+
+            if(Physics.Raycast(left, transform.forward, out hit, leftRay))
+            {
+                raycastOffset += Vector3.up;
+            }
+            else if(Physics.Raycast(right, transform.forward, out hit, rayDistance))
+            {
+                Debug.Log("Ray collision2");
+                raycastOffset -= Vector3.up;
+            }
+
+            if (Physics.Raycast(forward, transform.forward, out hit, rayDistance))
+            {
+                
+            }
+            else if (Physics.Raycast(backward, -transform.forward, out hit, rayDistance))
+            {
+                
+            }
+
+            if(raycastOffset != Vector3.zero)
+            {
+                transform.Rotate(raycastOffset * rotate * Time.deltaTime);
+            }
+            else
+            {
+                Turn();
+            }
         }
         else
         {
-            Turn();
+            RaycastHit hit;
+            destination = toChase.transform.position;
+            distanceToTarget = (destination - transform.position).magnitude;
+            if (distanceToTarget < 0.15) Debug.Log("case 1"); //attack
+            else if (distanceToTarget < 0.4f && Physics.Linecast(transform.position, destination, out hit))
+            {
+                if (hit.transform.name == "Target")
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, destination, speed*Time.deltaTime);
+                    Debug.Log(toChase.transform.position);
+                    Debug.Log("ww" + destination);
+                }
+            }
+            else
+            {
+                transform.Translate(direction * speed * Time.deltaTime);
+                Debug.Log("case 3");
+            }
         }
     }
 }
