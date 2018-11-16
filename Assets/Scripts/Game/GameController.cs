c5795a9f5808e7e229f4598369c4221b8ea26fb3
Samuel Proulx <proulxsamuel@gmail.com>
2018-11-16 18:47:23 -0500

    Make power up chest spawn upon boss defeat

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -112,13 +112,19 @@ public class GameController : MonoBehaviour {
 		hud.DisplayBossHealth();
 		yield return new WaitUntil(() => boss == null);
 		hud.HideBossHealth();
-		yield return new WaitForSeconds(2.0f);
+		yield return new WaitForSeconds(0.25f);
 		if (numberOfFloors > 0) {
 			var centerPosition = currentRoom.centerPosition;
-			var effectPosition = LayoutGrid.ToWorldPosition(centerPosition, true);
+			var effectPosition = LayoutGrid.ToWorldPosition(centerPosition + new Vector2Int(0, 1), true);
             AudioSource.PlayClipAtPoint(terrain.breakSoundEffect, effectPosition, terrain.soundVolume);
             Instantiate(terrain.breakEffect, effectPosition, Quaternion.identity);
-			terrain.Place(terrain.terrainBlocks.passage, centerPosition);
+			terrain.PlaceProp(terrain.terrainProps.chest, centerPosition + new Vector2Int(0, 1));
+			
+			yield return new WaitForSeconds(4.0f);
+			effectPosition = LayoutGrid.ToWorldPosition(centerPosition - new Vector2Int(0, 1), true);
+            AudioSource.PlayClipAtPoint(terrain.breakSoundEffect, effectPosition, terrain.soundVolume);
+            Instantiate(terrain.breakEffect, effectPosition, Quaternion.identity);
+			terrain.Place(terrain.terrainBlocks.passage, centerPosition - new Vector2Int(0, 1));
 		} else {
 			StartCoroutine(Victory());
 		}
